c4407537fb9af0e3f4439c0c8bf2d21f46dcf74e
Sky Moore <mskymoore@gmail.com>
2020-07-25 18:57:47 -0500

    add model inlines show relationships admin panel

--- src/temapi/admin.py
+++ src/temapi/admin.py
@@ -4,16 +4,48 @@ from django.contrib import admin
 from .models import EntryDate, Discipline, Position
 from .models import Employee, Client, Region, Site, DayRate
 from .models import Rate, Equipment, EquipmentCharge, ManHoursCharge
+from .models import Worklog, Dispute, RateSheet
+
+
+class ManHoursChargeAdminTab(admin.TabularInline):
+    model = ManHoursCharge
+
+class EquipmentChargeAdminTab(admin.TabularInline):
+    model = EquipmentCharge
+
+class WorklogAdminTab(admin.TabularInline):
+    model = Worklog
+
+class WorklogAdmin(admin.ModelAdmin):
+   inlines = [ManHoursChargeAdminTab, EquipmentChargeAdminTab]
+
+class EquipmentAdmin(admin.ModelAdmin):
+    inlines = [EquipmentChargeAdminTab,]
+
+class EmployeeAdmin(admin.ModelAdmin):
+    inlines = [ManHoursChargeAdminTab,]
+
+class PositionAdmin(admin.ModelAdmin):
+    inlines = [ManHoursChargeAdminTab,]
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [WorklogAdminTab,]
+
+class SiteAdmin(admin.ModelAdmin):
+    inlines = [WorklogAdminTab,]
 
 admin.site.register(EntryDate)
 admin.site.register(Discipline)
-admin.site.register(Position)
-admin.site.register(Employee)
-admin.site.register(Client)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Region)
-admin.site.register(Site)
+admin.site.register(Site, SiteAdmin)
 admin.site.register(Rate)
 admin.site.register(DayRate)
-admin.site.register(Equipment)
+admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(EquipmentCharge)
 admin.site.register(ManHoursCharge)
+admin.site.register(RateSheet)
+admin.site.register(Worklog, WorklogAdmin)
+admin.site.register(Dispute)
