a7f19a645f062eda078dda00c1bceec11ed502fd
Sky Moore <mskymoore@gmail.com>
2020-07-28 02:02:00 -0500

    add session auth, theres' no difference

--- src/djreact/settings.py
+++ src/djreact/settings.py
@@ -237,6 +237,7 @@ if DEBUG:
 else:
     REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework.authentication.SessionAuthentication',
             'rest_framework.authentication.TokenAuthentication',
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
